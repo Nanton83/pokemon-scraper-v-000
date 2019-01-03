6a229a78bd7e1ae7ade84314a4c4bd474c08f7e6
Nick Romans <nicholas.romans@gmail.com>
2019-01-03 02:01:37 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,7 +22,7 @@ end
 
 def self.find(id, db)
 pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [id]).flatten
-new_pokemon = self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+new_pokemon = self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
 end
 
 
