3086cb3c0f40419c1463cff695b614e1d057b46b
Nick Romans <nicholas.romans@gmail.com>
2019-01-03 01:52:07 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,30 +6,30 @@ class Pokemon
 
 
 
-def initialize(id:, name:, type:, db:, hp:)
+def initialize(id:, name:, type:, db:)
   @id = id
   @name = name
   @type = type
-  @hp = hp
+  
 end
 
-def self.save(name, type, db, hp)
-db.execute("INSERT INTO pokemon (name, type, hp) VALUES (?, ?, ?)",name, type, hp)
+def self.save(name, type, db)
+db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
 end
 
 
 def self.find(id, db)
 pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [id]).flatten
-new_pokemon = self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db, hp: pokemon[3])
+new_pokemon = self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
 end
 
 def self.find(id_num, db)
 db.execute("SELECT * FROM pokemon WHERE id = ?", [id_num])
 end
 
-def alter_hp(num, db)
-   db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
-   self.hp = num
- end
+# def alter_hp(num, db)
+#    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
+#    self.hp = num
+#  end
 
 end
